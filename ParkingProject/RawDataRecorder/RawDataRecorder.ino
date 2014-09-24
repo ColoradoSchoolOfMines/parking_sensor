c0e3a6a48e7934f8556daddeadbe49fe35818723
Martin Kuchta <makuchta@mines.edu>
2014-09-23 19:01:17 -0600

    Add 9.23.14 car data

--- ParkingProject/RawDataRecorder/RawDataRecorder.ino
+++ ParkingProject/RawDataRecorder/RawDataRecorder.ino
@@ -18,8 +18,7 @@ Arduino, HMC5883, magnetometer, XBee code
  Developer: Ted Ullricis_measuringh <ted@tomorrow-lab.com>
  http://tomorrow-lab.com
  
- A
- */
+*/
 
 #include <Wire.h> //I2C Arduino Library
 #include <EEPROM.h>
@@ -44,7 +43,7 @@ int x,y,z; //triple axis data
 
 void setup(){
   //Initialize Serial and I2C communications
-  Serial.begin(57600);
+  Serial.begin(9600);
   Wire.begin();
 //  StatsX.clear(); //clear the stat list
 //  StatsY.clear();
@@ -86,17 +85,16 @@ void loop(){
   Wire.beginTransmission(address);
   Wire.write(0x03); //select register 3, X MSB register
   Wire.endTransmission();
-
-
+  
   //Read data from each axis, 2 registers per axis
   Wire.requestFrom(address, 6);
   if(6<=Wire.available()){
     x = Wire.read()<<8; //X msb
     x |= Wire.read(); //X lsb
-    z = Wire.read()<<8; //Z msb
-    z |= Wire.read(); //Z lsb
     y = Wire.read()<<8; //Y msb
     y |= Wire.read(); //Y lsb
+    z = Wire.read()<<8; //Z msb
+    z |= Wire.read(); //Z lsb
   }
 
 //  StatsX(pointer)= x;  //once array is at 300, start re-using data slots
